4a5ecd50db60a213470be0d989411379d7059006
ahaxu <banghahatinh96@gmail.com>
2022-08-28 21:33:39 +0700

    Make color

--- x.sh
+++ x.sh
@@ -6,26 +6,26 @@ ship() {
     git commit -am "$COMMIT_MSG"
     git push origin main
 
-    echo "sleep abit for page rendering ...!"
+    echo "λ> sleep abit for page rendering ...!"
     sleep 10
 
-    echo "https://ahaxu.github.io/ updated!"
+    echo "λ> https://ahaxu.github.io/ updated!"
 }
 
 COMMIT_MSG=$1
 
-echo "cleaning ..."
+echo "λ> cleaning ..."
 stack exec ahaxu-blog clean
 
-echo "building ..."
+echo "λ> building ..."
 stack exec ahaxu-blog build
 
 GIT_STATUS=$(git status 2>&1 | grep "nothing to commit")
 if [[ -z "$GIT_STATUS" ]]; then
-  echo "shipping to prod ..."  
+  echo "λ> shipping to prod ..."  
   ship
 else
-  echo "nothing to do !"
+  echo "λ> nothing to do !"
 fi
 exit 1
 
