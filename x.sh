3c563c78309ebd9ba1d7a5e69f6474081b4e876e
ahaxu <banghahatinh96@gmail.com>
2022-08-28 20:56:18 +0700

    Update deploy script

--- x.sh
+++ x.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# ship function
+ship() {
+    git add .
+    git commit -am "$COMMIT_MSG"
+    git push origin main
+
+    echo "sleep abit for page rendering ...!"
+    sleep 10
+
+    echo "https://ahaxu.github.io/ updated!"
+}
+
 COMMIT_MSG=$1
 
 echo "cleaning ..."
@@ -8,15 +20,12 @@ stack exec ahaxu-blog clean
 echo "building ..."
 stack exec ahaxu-blog build
 
-# todo check if any update from local
-# git status
-
-git add .
-git commit -am "$COMMIT_MSG"
-git push origin main
-
-echo "sleep abit for page rendering ...!"
-sleep 10
-
-echo "https://ahaxu.github.io/ updated!"
+GIT_STATUS=$(git status 2>&1 | grep "nothing to commit")
+if [[ -z "$GIT_STATUS" ]]; then
+  echo "shipping to prod ..."  
+  ship
+else
+  echo "nothing to do !"
+fi
+exit 1
 
